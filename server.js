61501eac6da159590e117a0fe27912bb344cbdf3
mario_chan15 <mario.luiz.miranda@gmail.com>
2018-05-08 18:07:26 -0300

    S05-C45 - Advanced Template w/ Handlebars

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
-const hbs = require('hbs');
+const hbs = require('hbs'); // use of handlebars for templates
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => { // use of middleware to log, like a filter
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// })
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
