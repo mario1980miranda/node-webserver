9d62735486ff8a2e41d52dfc5b6aad64b3030d55
mario_chan15 <mario.luiz.miranda@gmail.com>
2018-05-08 21:00:33 -0300

    S05-C49 - Adding a new feature and deploynig at heroku

--- server.js
+++ server.js
@@ -44,8 +44,14 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
+        pageTitle: 'About Page'
     });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    })
 })
 
 app.get('/bad', (req, res) => {
